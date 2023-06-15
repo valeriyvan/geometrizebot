c30bd41d66221c68a49aa491a429a7a9366280a2
Valeriy Van <github@w7software.com>
2023-06-15 15:54:39 +0300

    Copypaste entrypoint code create with vapor new hello -n.
    This rapaired HTTP working on localhost.

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -10,12 +10,40 @@ let tgPort: Int = ProcessInfo.processInfo.environment["geometrizebot_port"].flat
 // nil s3Bucket means no debug upload of source images
 let s3Bucket: String? = ProcessInfo.processInfo.environment["debug_upload_images_into_s3_bucket"]
 
+// This extension is temporary and can be removed once Vapor gets this support.
+// This is from project created with `vapor new hello -n`.
+private extension Vapor.Application {
+    static let baseExecutionQueue = DispatchQueue(label: "vapor.codes.entrypoint")
+
+    func runFromAsyncMainEntrypoint() async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            Vapor.Application.baseExecutionQueue.async { [self] in
+                do {
+                    try self.run()
+                    continuation.resume()
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
+
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
-let eventLoop: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount * 4)
-let app: Application = .init(env, Application.EventLoopGroupProvider.shared(eventLoop))
-let TGBOT: TGBotConnection = .init()
+let eventLoopGroup: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount * 4)
+let app: Application = Application(
+    env,
+    Application.EventLoopGroupProvider.shared(eventLoopGroup)
+)
+let TGBOTCONNECTION: TGBotConnection = TGBotConnection()
 
 defer { app.shutdown() }
-try await configure(app)
-try app.run()
+
+do {
+    try await configure(app)
+} catch {
+    app.logger.report(error: error)
+    throw error
+}
+try await app.runFromAsyncMainEntrypoint()
