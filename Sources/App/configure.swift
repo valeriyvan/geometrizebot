615e0e7b7f7d566d0493d55e2a099a9463627d0c
Valeriy Van <github@w7software.com>
2023-06-16 11:06:20 +0300

    Add geometrizing images uploaded over web
    Parameters of geometrizing are hardcoded by now.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,12 +1,19 @@
 import Vapor
+import Leaf
 import TelegramVaporBot
 
 public func configure(_ app: Application) async throws {
     app.http.server.configuration.hostname = tgHostname
     app.http.server.configuration.port = tgPort
 
+    app.routes.defaultMaxBodySize = "10mb"
+
     // Serves files from `Public/` directory
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    // setup Leaf template engine
+    // LeafRenderer.Option.caching = .bypass
+    app.views.use(.leaf)
 
     /// set level of debug if you needed
     TGBot.log.logLevel = app.logger.logLevel
