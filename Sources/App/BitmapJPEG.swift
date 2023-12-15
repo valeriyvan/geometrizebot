fee7fc683bcd6b1935c900f1f4f628b07cc7f0ff
Valeriy Van <github@w7software.com>
2023-12-15 21:08:14 +0200

    Reuse bitmap rotation from swift-geometrize

--- Sources/App/BitmapJPEG.swift
+++ Sources/App/BitmapJPEG.swift
@@ -12,45 +12,8 @@ extension Bitmap {
         let (width, height) = image.size
         let rgba: [UInt8] = rgb.flatMap({ [$0.r, $0.g, $0.b, 255] })
         var bitmap = Bitmap(width: width, height: height, data: rgba)
-        for m in image.metadata {
-            switch m {
-            case .exif(let exif):
-                if let orientationTag = exif[tag: 0x112] {
-                    let orientation = orientationTag.box.endianness == .littleEndian ? orientationTag.box.contents.0 : orientationTag.box.contents.1
-                    switch orientation {
-                        // https://home.jeita.or.jp/tsc/std-pdf/CP3451C.pdf, page 30
-                    case 1: // 1 The Oth row is at the visual top of the image, and the 0th column is the visual left-hand side.
-                        // normal orientation
-                        ()
-                    case 2: // 2 The Oth row is at the visual top of the image, and the Oth column is the visual right-hand side.
-                        bitmap.reflectVertically()
-                    case 3: // 3 The Oth row is at the visual bottom of the image, and the Oth column is the visual right-hand side.
-                        bitmap.reflectHorizontally()
-                    case 4: // 4 The Oth row is at the visual bottom of the image, and the 0th column is the visual left-hand side.
-                        bitmap.reflectHorizontally()
-                        bitmap.reflectVertically()
-                    case 5: // 5 The Oth row is the visual left-hand side of the image, and the 0th column is the visual top.
-                        bitmap.transpose()
-                        bitmap.transpose()
-                        bitmap.transpose()
-                        bitmap.reflectVertically()
-                        bitmap.reflectHorizontally()
-                    case 6: // 6 The Oth row is the visual right-hand side of the image, and the 0th column is the visual top.
-                        bitmap.transpose()
-                        bitmap.reflectVertically()
-                    case 7: // 7 The Oth row is the visual right-hand side of the image, and the 0th column is the visual bottom.
-                        bitmap.transpose()
-                        bitmap.reflectVertically()
-                    case 8: // 8 The Oth row is the visual left-hand side of the image, and the 0th column is the visual bottom.
-                        bitmap.transpose()
-                    default:
-                        ()
-                    }
-                }
-            default:
-                ()
-            }
-        }
+        let exifOrientation = Bitmap.ExifOrientation(rawValue: image.exifOrientation()) ?? .up
+        bitmap.rotateToUpOrientation(accordingTo: exifOrientation)
         self = bitmap
     }
 }
@@ -69,4 +32,23 @@ private struct DataBytestreamSource: _JPEGBytestreamSource {
         defer { offset += count }
         return Array(data[offset ..< offset + count])
     }
+}
+
+extension JPEG.Data.Rectangular<JPEG.Common> {
+
+    func exifOrientation() -> Int {
+        for m in metadata {
+            switch m {
+            case .exif(let exif):
+                if let orientationTag = exif[tag: 0x112] {
+                    let orientation = orientationTag.box.endianness == .littleEndian ? orientationTag.box.contents.0 : orientationTag.box.contents.1
+                    return Int(orientation)
+                }
+            default:
+                ()
+            }
+        }
+        return 1
+    }
+
 }
