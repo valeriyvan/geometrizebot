30f671142070bccdabb9bd12d2664ba95c2f0240
Valeriy Van <github@w7software.com>
2023-12-30 00:01:12 +0200

    Remove commented out code

--- Sources/App/Geometrizer.swift
+++ Sources/App/Geometrizer.swift
@@ -62,9 +62,6 @@ struct SVGIterator: AsyncIteratorProtocol {
             targetBitmap = bitmap.downsample(factor: maxSize / downscaleSize)
         }
 
-        //targetBitmap.transpose()
-        //targetBitmap.reflectVertically()
-
         width = targetBitmap.width
         height = targetBitmap.height
 
